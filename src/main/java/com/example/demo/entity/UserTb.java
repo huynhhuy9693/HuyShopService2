9c09ae164805007e4d56169c9fc16ec0a045a477
huynhhuy9693 <huyhuynh9693@gmail.com>
2022-06-24 00:59:00 +0700

    admin_user

---
+++ src/main/java/com/example/demo/entity/UserTb.java
@@ -0,0 +1,54 @@
+package com.example.demo.entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.sql.Date;
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class UserTb {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    String name;
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    @Column(name = "dob")
+    Date dob;
+    @Column(name = "phone")
+    String phone;
+    @Column(name = "email")
+    String email;
+    @Column(name = "address")
+    String address;
+    @Column(name = "status")
+    boolean status;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "pass_word")
+    private String passWord;
+//    @Column(name = "role")
+//    private int role;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role roleId;
+
+//    @OneToOne(mappedBy = "user")
+//    private Cart cart;
+
+    @PrePersist
+    void onPrePersist() {
+        if (status==false) {
+            status=true;
+        }
+    }
+}
